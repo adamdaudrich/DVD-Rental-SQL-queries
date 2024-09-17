e7e7c919b199549bb5ac1216a85bfac47c018775
a_daud <adam.daudrich@pm.me>
2024-09-17 11:23:34 -0400

    Update and rename sales by category 2007.sql to queries.sql

---
+++ queries.sql
@@ -0,0 +1,45 @@
+SELECT category.name AS category, SUM(amount) AS total_sales
+FROM payment
+INNER JOIN rental USING(rental_id)
+INNER JOIN inventory ON inventory.inventory_id = rental.inventory_id
+INNER JOIN film ON inventory.film_id = film.film_id
+INNER JOIN film_category ON film.film_id = film_category.film_id
+INNER JOIN category ON category.category_id = film_category.category_id
+WHERE payment.payment_date >= '2007-01-01 00:00:00' 
+ AND  payment.payment_date <  '2008-01-01 00:00:00'
+GROUP BY category.name
+ORDER BY sum(amount) DESC;
+
+SELECT film.title, film.rental_rate, SUM(amount) AS total_amount
+FROM payment
+INNER JOIN rental USING(rental_id)
+INNER JOIN inventory USING(inventory_id)
+INNER JOIN film USING(film_id)
+GROUP BY film.title, film.rental_rate 
+ORDER BY total_amount DESC 
+LIMIT 3;
+
+SELECT
+    EXTRACT (YEAR FROM rental_date) y,
+    EXTRACT (MONTH FROM rental_date) M,
+    EXTRACT (DAY FROM rental_date) d,
+    COUNT (rental_id)
+FROM
+    rental
+GROUP BY
+    ROLLUP (
+        EXTRACT (YEAR FROM rental_date),
+        EXTRACT (MONTH FROM rental_date),
+        EXTRACT (DAY FROM rental_date));
+	
+WITH action_films AS (
+  SELECT 
+    f.title, f.length 
+  FROM 
+    film f 
+    INNER JOIN film_category fc USING (film_id) 
+    INNER JOIN category c USING (category_id) 
+  WHERE 
+    c.name = 'Action') 
+SELECT * FROM action_films;
+
