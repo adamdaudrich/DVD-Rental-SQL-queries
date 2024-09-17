844e39d9f8fb91b0b7684e025910bc796f4a6093
a_daud <adam.daudrich@pm.me>
2024-09-17 11:24:59 -0400

    Delete docs under 60 mins.sql

--- docs under 60 mins.sql
+++
@@ -1,7 +0,0 @@
-SELECT f.title, f.length, c.name AS category
-FROM film f
-INNER JOIN film_category fc USING (film_id)
-INNER JOIN category c USING (category_id)
-WHERE c.name = 'Documentary' AND f.length < 60
-ORDER BY title
-;
