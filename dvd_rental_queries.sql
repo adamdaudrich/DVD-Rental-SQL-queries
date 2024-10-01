ab2e7d2ed995261307a34a8a4a8a83dc13c610dd
a_daud <adam.daudrich@pm.me>
2024-10-01 10:57:11 -0400

    Update dvd_rental_queries.sql

--- dvd_rental_queries.sql
+++ dvd_rental_queries.sql
@@ -20,27 +20,5 @@ GROUP BY film.title, film.rental_rate
 ORDER BY total_amount DESC 
 LIMIT 3;
 
-SELECT
-    EXTRACT (YEAR FROM rental_date) y,
-    EXTRACT (MONTH FROM rental_date) M,
-    EXTRACT (DAY FROM rental_date) d,
-    COUNT (rental_id)
-FROM
-    rental
-GROUP BY
-    ROLLUP (
-        EXTRACT (YEAR FROM rental_date),
-        EXTRACT (MONTH FROM rental_date),
-        EXTRACT (DAY FROM rental_date));
-	
-WITH action_films AS (
-  SELECT 
-    f.title, f.length 
-  FROM 
-    film f 
-    INNER JOIN film_category fc USING (film_id) 
-    INNER JOIN category c USING (category_id) 
-  WHERE 
-    c.name = 'Action') 
-SELECT * FROM action_films;
+
 
